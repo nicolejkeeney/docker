ae229f8d138a54f3298b729793757f2a71bb510b
Nicole Keeney <nicolejkeeney@gmail.com>
2023-08-11 18:12:42 -0600

    update imports for new img

--- test/py_test.py
+++ test/py_test.py
@@ -1,10 +1,15 @@
-# python script to test imports 
+# python script to test image
 
+# A few libraries in the environment file 
 import xarray as xr 
 import numpy as np 
 import pandas as pd
 import scipy
+import matplotlib.pyplot as plt
+import hvplot.pandas
+import hvplot.xarray
 
+# Make a dummy xr.DataArray object 
 data_np = np.arange(0,10,1)
 da = xr.DataArray(data_np, coords={"x":np.arange(0,10,1)})
 print("xr.DataArray: {0}".format(da))
